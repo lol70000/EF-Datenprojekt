6ae270e68a3d9652d53e69fae394c3658599471e
lol70000 <113106861+lol70000@users.noreply.github.com>
2022-11-24 14:41:05 +0100

    Add files via upload
    final project beginning

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
-<h1> MySQL go Br</h1>
+<h1>Welcome to my EF5_Databrojekt</h1>
 <?php
 
 $servername = "localhost";
 $username = "root";
 $password = "root";
+
+//Connecting to Server and Setting Error Trap
 try{
-    include "delete.php";
-    include "create.php";
-    $link = new PDO("mysql:host=$servername;dbname=ef5_base", $username, $password);
-    $link->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    
-
-    $insert_beat = $link->prepare("INSERT INTO lehrperson(first_name, last_name ,schulfach) Values('Beat','Temperli','1')");
-    $lol = $link->prepare("INSERT INTO schulfach(name) Values('Informatik')");
-    $insert_Timon = $link->prepare("INSERT INTO lehrperson(first_name, last_name ,schulfach) Values('Timon','Ruther','1')");
-
-    $lol->execute();
-    $insert_beat->execute();
-    $insert_Timon->execute();
+    $conn = new PDO("mysql:host=$servername;dbname=ef5_proj",$username,$password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo "Connected!<br>";
 }catch(PDOException $e){
     echo "connection failed.<br>" . $e -> getmessage();
 }
+
+$createTableMaterial ='
+    CREATE TABLE material(
+        id_material INT AUTO_INCREMENT,
+        name VARCHAR(100) NOT NULL,
+        anzahl_DMX TINYINT NOT NULL,
+        watt_draw TINYINT NOT NULL,
+        anzahl TINYINT NOT NULL,
+    );';
