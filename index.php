a8f20e1a68ed4a709b0f83225966284e65cb404c
lol70000 <113106861+lol70000@users.noreply.github.com>
2022-11-27 22:03:08 +0100

    Add files via upload
    created all tables and included an external delete and index file.

--- index.php
+++ index.php
@@ -9,6 +9,13 @@ try{
     include "create.php";
     $link = new PDO("mysql:host=$servername;dbname=ef5_proj", $username, $password);
     $link->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    $place = [Turnhalle, Robotic, Aula];
+    foreach ($place as $number){
+        $lol = $link->prepare("INSERT INTO place(name) Values('$number')");
+    }
+        
+
 }catch(PDOException $e){
     echo "connection failed.<br>" . $e -> getmessage();
 }
