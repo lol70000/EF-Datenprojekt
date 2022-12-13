2b5c2eb94fea7f430f60118d63d3f201d7b3c614
lol70000 <113106861+lol70000@users.noreply.github.com>
2022-12-13 15:24:22 +0100

    Add files via upload

--- form.php
+++ form.php
@@ -5,14 +5,70 @@
     </head>
 
     <body>
+        <a href="http://localhost/us_opt1/db_structure.php?server=1&db=ef5_proj" target="_blank">php MyAdmin in order to see indecies</a>
 
         <h1>Formular</h1>
 
         <form action="insert.php" method="POST">
-            <label for="pl_name">Place Name</label>
-            <br><input type="text" id="pl_name" name="pla_name">
-            <br><label for="ln_name">Abkürzung</label>
-            <br><input type="text" id="sf_abkz" name="sf_abkz">
+            <label for="pl_name">Name of the Place</label>
+            <br><input type="text" id="pl_name" name="pl_name">
+            <hr>
+
+            <br><label for="ln_name">Name of the Lender</label>
+            <br><input type="text" id="ln_name" name="ln_name">
+
+            <br><label for="ln_money">Money owed to the Lender</label>
+            <br><input type="number" id="ln_money" name="ln_money">
+            <hr>
+
+            <br><label for="ma_name">Material Name</label>
+            <br><input type="text" id="ma_name" name="ma_name">
+
+            <br><label for="ma_dmx">Anzahl Dmx (Des Oben angegebenen Materials)</label>
+            <br><input type="number" id="ma_dmx" name="ma_dmx">
+
+            <br><label for="ma_watt">Watt draw (Des Oben angegebenen Materials)</label>
+            <br><input type="number" id="ma_watt" name="ma_watt">
+
+            <br><label for="ma_con_type">Connection Type(Des Oben angegebenen Materials)</label>
+            <br><input type="text" id="ma_con_type" name="ma_cn_type">
+
+            <br><label for="ma_anz">Anzahl(Des Oben angegebenen Materials)</label>
+            <br><input type="number" id="ma_anz" name="ma_anz">
+
+            <br><label for="ma_place">Platz wohin es muss (Des Oben angegebenen Materials)</label>
+            <br><input type="number" id="ma_place" name="ma_place">
+
+            <br><label for="ma_lender">Wer es ausgeliehen hat(Des Oben angegebenen Materials)</label>
+            <br><input type="number" id="ma_lender" name="ma_lender">
+            <hr>
+
+            <br><label for="sh_name">Name der schicht</label>
+            <br><input type="text" id="sh_name" name="sh_name">
+
+            <br><label for="sh_beginn">Start Zeit der Schicht</label>
+            <br><input type="number" id="sh_beginn" name="sh_beginn">
+
+            <br><label for="sh_end">Ende der Schicht</label>
+            <br><input type="number" id="sh_end" name="sh_end">
+            <hr>
+
+            <br><label for="op_name">Name der schicht</label>
+            <br><input type="text" id="op_name" name="op_name">
+
+            <br><label for="op_job">Name der schicht</label>
+            <br><input type="number" id="op_job" name="op_job">
+            <hr>
+
+            <br><label for="con_op">Name der schicht</label>
+            <br><input type="number" id="con_op" name="con_op">
+
+            <br><label for="con_sh">Name der schicht</label>
+            <br><input type="number" id="con_sh" name="con_sh">
+
+            <br><label for="con_pl">Name der schicht</label>
+            <br><input type="number" id="con_pl" name="con_pl">
+
             <br><input type="submit" value="absenden">
         </form>
     </body>
--- insert.php
+++ insert.php
@@ -7,23 +7,101 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=ef5:proj", $user, $pw);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo "Connected!<br>";
+    echo "<p style='color:rgb(87, 119, 143)'>Connected!<br></p>";
 } catch (PDOException $e) {
-    echo "Connection failed. " . $e->getMessage();
-}
+    echo "<p style='color:rgb(87, 119, 143)'>Connection failed. " . $e->getMessage()."</p>";
+};
 
 
-$name_place = $_POST['pla_name'];
+$name_place = $_POST['pl_name'];
 
 if ($name_place != '') {
-    $insertIntoPlace= $conn->prepare("INSERT INTO place(name) VALUES('$name_place')");
+    $insertIntoPlace = $conn->prepare("INSERT INTO place(name) VALUES('$name_place')");
 
     try {
         $conn->exec($insertIntoPlace);
-        echo "Daten eingefügt.<br>";
+        echo "<p style='color:rgb(87, 119, 143)'>Place eingefügt.<br></p>";
     } catch (PDOException $e) {
-        echo "Insertion failed: " . $e->getMessage();
+        echo "<p style='color:rgb(87, 119, 143)'>place has failed: " . $e->getMessage(). "<br></p>";
     }
 };
+
+$name_lender = $_POST['ln_name'];
+$money_lender = $_POST['ln_money'];
+
+if ($name_lender != '' && $money_lender != ''){
+    $insertIntoLender = $conn->prepare("INSERT INTO lender(name, money_owed) VALUES('$name_lender',$money_lender");
+
+    try{
+        $conn->exec($insertIntoLender);
+        echo "<p style='color:rgb(87, 119, 143)'>Lender eingefügt.<br></p>";
+    }catch(PDOException $e){
+        echo "<p style='color:rgb(87, 119, 143)'>Lender has failed: " . $e->getMessage(). "<br></p>";
+    };
+};
+
+$name_material = $_POST['ma_name'];
+$dmx_material = $_POST['ma_dmx'];
+$watt_material = $_POST['ma_watt'];
+$conn_material = $_POST['ma_con_type'];
+$anzahl_material = $_POST['ma_anz'];
+$place_material = $_POST['ma_place'];
+$lender_material = $_POST['ma_lender'];
+
+if ($name_material != '' && $dmx_material != '' && $watt_material != '' && $conn_material != '' && $anzahl_material != '' && $place_material != '' && $lender_material!= ''){
+    $insertIntoMaterial = $conn->prepare("INSERT INTO material(name, anzahl_DMX, watt_draw, connection_type, anzahl, place, lender) VALUES($name_material, $dmx_material, $watt_material, $conn_material, $anzahl_material, $place_material, $lender_material)");
+
+    try{
+        $conn->exec($insertIntoMaterial);
+        echo "<p style='color:rgb(87, 119, 143)'>Material eingefügt.<br></p>";
+    }catch(PDOException $e){
+        echo "<p style='color:rgb(87, 119, 143)'>Material has failed: " . $e->getMessage()."<br></p>";
+    };
+};
+
+$name_schicht = $_POST['sh_name'];
+$beginn_schicht = $_POST['sh_beginn'];
+$end_schicht = $_POST['sh_end'];
+
+if ($name_schicht != '' && $beginn_schicht != '' && $end_schicht != ''){
+    $insertIntoSchicht = $conn->prepare("INSERT INTO schicht(name, beginn, end) VALUES($name_schicht, $beginn_schicht, $end_schicht)");
+
+    try{
+        $conn->exec($insertIntoSchicht);
+        echo "<p style='color:rgb(87, 119, 143)'>Schicht eingefügt.<br></p>";
+    }catch(PDOException $e){
+        echo "<p style='color:rgb(87, 119 ,143)'>Schicht has failed: " . $e->getMessage(). "<br></p>";
+    };
+};
+
+$name_operator = $_POST['op_name'];
+$job_operator = $_POST['op_job'];
+
+if ($name_operator != '' && $job_operator != ''){
+    $insertIntoOperator = $conn->prepare("INSERT INTO operator(name, job) VALUES($name_operator, $job_operator)");
+
+    try{
+        $conn->exec($insertIntoOperator);
+        echo "<p style='color:rgb(87, 119, 143)>Operator eingefügt.<br></p>";
+    }catch(PDOException $e){
+        echo "<p style='color:rgb(87, 119, 143)>Operator has failed: " . $e->getMessage() . "<br></p>";
+    };
+};
+
+$operator_connection = $_POST['con_op'];
+$schicht_connection = $_POST['con_sh'];
+$place_connection = $_POST['con_pl'];
+
+if ($operator_connection != '' && $schicht_connection != '' && $place_connection != ''){
+    $insertIntoConnection = $conn->prepare("INSERT INTO(op, schicht, place) VALUES($operator_connection, $schicht_connection, $place_connection)");
+
+    try{
+        $conn->exec($insertIntoConnection);
+        echo "<p style='color:rgb(87, 119 ,143)>Connection eingefügt.<br></p>";
+    }catch(PDOException $e){
+        echo "<p style='color:rgb(87, 119, 143)>Connection has failed: ". $e->getMessage() . "<br></p>";
+    };
+};
+
 
 ?>
