67fad5cc3aa87dba5ae20b8fc4e367684f76a155
lol70000 <113106861+lol70000@users.noreply.github.com>
2022-12-13 16:24:32 +0100

    Add files via upload

--- form.php
+++ form.php
@@ -4,69 +4,69 @@
         <title>Datenbankformular</title>
     </head>
 
-    <body>
+    <body style="background-color:rgba(126,74,28,0.339);">
         <a href="http://localhost/us_opt1/db_structure.php?server=1&db=ef5_proj" target="_blank">php MyAdmin in order to see indecies</a>
 
-        <h1>Formular</h1>
+        <h1 style="color:rgb(87, 119, 143)">Formular</h1>
 
         <form action="insert.php" method="POST">
-            <label for="pl_name">Name of the Place</label>
+            <label for="pl_name" style="color:rgb(87, 119, 143)">Name of the Place</label>
             <br><input type="text" id="pl_name" name="pl_name">
             <hr>
 
-            <br><label for="ln_name">Name of the Lender</label>
+            <br><label for="ln_name" style="color:rgb(87, 119, 143)">Name of the Lender</label>
             <br><input type="text" id="ln_name" name="ln_name">
 
-            <br><label for="ln_money">Money owed to the Lender</label>
+            <br><label for="ln_money" style="color:rgb(87, 119, 143)">Money owed to the Lender</label>
             <br><input type="number" id="ln_money" name="ln_money">
             <hr>
 
-            <br><label for="ma_name">Material Name</label>
+            <br><label for="ma_name" style="color:rgb(87, 119, 143)">Material Name</label>
             <br><input type="text" id="ma_name" name="ma_name">
 
-            <br><label for="ma_dmx">Anzahl Dmx (Des Oben angegebenen Materials)</label>
+            <br><label for="ma_dmx" style="color:rgb(87, 119, 143)">Anzahl Dmx (Des Oben angegebenen Materials)</label>
             <br><input type="number" id="ma_dmx" name="ma_dmx">
 
-            <br><label for="ma_watt">Watt draw (Des Oben angegebenen Materials)</label>
+            <br><label for="ma_watt" style="color:rgb(87, 119, 143)">Watt draw (Des Oben angegebenen Materials)</label>
             <br><input type="number" id="ma_watt" name="ma_watt">
 
-            <br><label for="ma_con_type">Connection Type(Des Oben angegebenen Materials)</label>
-            <br><input type="text" id="ma_con_type" name="ma_cn_type">
+            <br><label for="ma_con_type" style="color:rgb(87, 119, 143)">Connection Type(Des Oben angegebenen Materials)</label>
+            <br><input type="text" id="ma_con_type" name="ma_con_type">
 
-            <br><label for="ma_anz">Anzahl(Des Oben angegebenen Materials)</label>
+            <br><label for="ma_anz" style="color:rgb(87, 119, 143)">Anzahl(Des Oben angegebenen Materials)</label>
             <br><input type="number" id="ma_anz" name="ma_anz">
 
-            <br><label for="ma_place">Platz wohin es muss (Des Oben angegebenen Materials)</label>
+            <br><label for="ma_place" style="color:rgb(87, 119, 143)">Platz wohin es muss (Des Oben angegebenen Materials)</label>
             <br><input type="number" id="ma_place" name="ma_place">
 
-            <br><label for="ma_lender">Wer es ausgeliehen hat(Des Oben angegebenen Materials)</label>
+            <br><label for="ma_lender" style="color:rgb(87, 119, 143)">Wer es ausgeliehen hat(Des Oben angegebenen Materials)</label>
             <br><input type="number" id="ma_lender" name="ma_lender">
             <hr>
 
-            <br><label for="sh_name">Name der schicht</label>
+            <br><label for="sh_name" style="color:rgb(87, 119, 143)">Name der schicht</label>
             <br><input type="text" id="sh_name" name="sh_name">
 
-            <br><label for="sh_beginn">Start Zeit der Schicht</label>
+            <br><label for="sh_beginn" style="color:rgb(87, 119, 143)">Start Zeit der Schicht</label>
             <br><input type="number" id="sh_beginn" name="sh_beginn">
 
-            <br><label for="sh_end">Ende der Schicht</label>
+            <br><label for="sh_end" style="color:rgb(87, 119, 143)">Ende der Schicht</label>
             <br><input type="number" id="sh_end" name="sh_end">
             <hr>
 
-            <br><label for="op_name">Name der schicht</label>
+            <br><label for="op_name" style="color:rgb(87, 119, 143)">Name des Bedieners</label>
             <br><input type="text" id="op_name" name="op_name">
 
-            <br><label for="op_job">Name der schicht</label>
-            <br><input type="number" id="op_job" name="op_job">
+            <br><label for="op_job" style="color:rgb(87, 119, 143)">Job des Bedieners</label>
+            <br><input type="text" id="op_job" name="op_job">
             <hr>
 
-            <br><label for="con_op">Name der schicht</label>
+            <br><label for="con_op" style="color:rgb(87, 119, 143)">Bediener (Index)</label>
             <br><input type="number" id="con_op" name="con_op">
 
-            <br><label for="con_sh">Name der schicht</label>
+            <br><label for="con_sh" style="color:rgb(87, 119, 143)">Schicht (Index)</label>
             <br><input type="number" id="con_sh" name="con_sh">
 
-            <br><label for="con_pl">Name der schicht</label>
+            <br><label for="con_pl" style="color:rgb(87, 119, 143)">Platz (Index</label>
             <br><input type="number" id="con_pl" name="con_pl">
 
             <br><input type="submit" value="absenden">
--- insert.php
+++ insert.php
@@ -4,7 +4,7 @@ $user = "root";
 $pw = "root";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=ef5:proj", $user, $pw);
+    $conn = new PDO("mysql:host=$servername;dbname=ef5_proj", $user, $pw);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     echo "<p style='color:rgb(87, 119, 143)'>Connected!<br></p>";
@@ -15,25 +15,25 @@ try {
 
 $name_place = $_POST['pl_name'];
 
-if ($name_place != '') {
+if ($name_place != NULL) {
     $insertIntoPlace = $conn->prepare("INSERT INTO place(name) VALUES('$name_place')");
 
     try {
-        $conn->exec($insertIntoPlace);
+        $insertIntoPlace->execute();
         echo "<p style='color:rgb(87, 119, 143)'>Place eingefügt.<br></p>";
     } catch (PDOException $e) {
         echo "<p style='color:rgb(87, 119, 143)'>place has failed: " . $e->getMessage(). "<br></p>";
-    }
+    };
 };
 
 $name_lender = $_POST['ln_name'];
 $money_lender = $_POST['ln_money'];
 
-if ($name_lender != '' && $money_lender != ''){
-    $insertIntoLender = $conn->prepare("INSERT INTO lender(name, money_owed) VALUES('$name_lender',$money_lender");
+if ($name_lender != NULL && $money_lender != NULL){
+    $insertIntoLender = $conn->prepare("INSERT INTO lender(name, money_owed) VALUES('$name_lender',$money_lender)");
 
     try{
-        $conn->exec($insertIntoLender);
+        $insertIntoLender->execute();
         echo "<p style='color:rgb(87, 119, 143)'>Lender eingefügt.<br></p>";
     }catch(PDOException $e){
         echo "<p style='color:rgb(87, 119, 143)'>Lender has failed: " . $e->getMessage(). "<br></p>";
@@ -48,11 +48,11 @@ $anzahl_material = $_POST['ma_anz'];
 $place_material = $_POST['ma_place'];
 $lender_material = $_POST['ma_lender'];
 
-if ($name_material != '' && $dmx_material != '' && $watt_material != '' && $conn_material != '' && $anzahl_material != '' && $place_material != '' && $lender_material!= ''){
-    $insertIntoMaterial = $conn->prepare("INSERT INTO material(name, anzahl_DMX, watt_draw, connection_type, anzahl, place, lender) VALUES($name_material, $dmx_material, $watt_material, $conn_material, $anzahl_material, $place_material, $lender_material)");
+if ($name_material != NULL && $dmx_material != NULL && $watt_material != NULL && $conn_material != NULL && $anzahl_material != NULL && $place_material != NULL && $lender_material!= NULL){
+    $insertIntoMaterial = $conn->prepare("INSERT INTO material(name, anzahl_DMX, watt_draw, connection_type, anzahl, place, lender) VALUES('$name_material', $dmx_material, $watt_material, '$conn_material' , $anzahl_material, $place_material, $lender_material)");
 
     try{
-        $conn->exec($insertIntoMaterial);
+        $insertIntoMaterial->execute();
         echo "<p style='color:rgb(87, 119, 143)'>Material eingefügt.<br></p>";
     }catch(PDOException $e){
         echo "<p style='color:rgb(87, 119, 143)'>Material has failed: " . $e->getMessage()."<br></p>";
@@ -63,11 +63,11 @@ $name_schicht = $_POST['sh_name'];
 $beginn_schicht = $_POST['sh_beginn'];
 $end_schicht = $_POST['sh_end'];
 
-if ($name_schicht != '' && $beginn_schicht != '' && $end_schicht != ''){
+if ($name_schicht != NULL && $beginn_schicht != NULL && $end_schicht != NULL){
     $insertIntoSchicht = $conn->prepare("INSERT INTO schicht(name, beginn, end) VALUES($name_schicht, $beginn_schicht, $end_schicht)");
 
     try{
-        $conn->exec($insertIntoSchicht);
+        $insertIntoSchicht->execute();
         echo "<p style='color:rgb(87, 119, 143)'>Schicht eingefügt.<br></p>";
     }catch(PDOException $e){
         echo "<p style='color:rgb(87, 119 ,143)'>Schicht has failed: " . $e->getMessage(). "<br></p>";
@@ -77,11 +77,11 @@ if ($name_schicht != '' && $beginn_schicht != '' && $end_schicht != ''){
 $name_operator = $_POST['op_name'];
 $job_operator = $_POST['op_job'];
 
-if ($name_operator != '' && $job_operator != ''){
-    $insertIntoOperator = $conn->prepare("INSERT INTO operator(name, job) VALUES($name_operator, $job_operator)");
+if ($name_operator != NULL && $job_operator != NULL){
+    $insertIntoOperator = $conn->prepare("INSERT INTO operator(name, job) VALUES('$name_operator',' $job_operator')");
 
     try{
-        $conn->exec($insertIntoOperator);
+        $insertIntoOperator->execute();
         echo "<p style='color:rgb(87, 119, 143)>Operator eingefügt.<br></p>";
     }catch(PDOException $e){
         echo "<p style='color:rgb(87, 119, 143)>Operator has failed: " . $e->getMessage() . "<br></p>";
@@ -92,11 +92,11 @@ $operator_connection = $_POST['con_op'];
 $schicht_connection = $_POST['con_sh'];
 $place_connection = $_POST['con_pl'];
 
-if ($operator_connection != '' && $schicht_connection != '' && $place_connection != ''){
+if ($operator_connection != NULL && $schicht_connection != NULL && $place_connection != NULL){
     $insertIntoConnection = $conn->prepare("INSERT INTO(op, schicht, place) VALUES($operator_connection, $schicht_connection, $place_connection)");
 
     try{
-        $conn->exec($insertIntoConnection);
+        $insertIntoConnection->execute();
         echo "<p style='color:rgb(87, 119 ,143)>Connection eingefügt.<br></p>";
     }catch(PDOException $e){
         echo "<p style='color:rgb(87, 119, 143)>Connection has failed: ". $e->getMessage() . "<br></p>";
